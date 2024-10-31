a78e2a1ddf0bb4792b0ad8aec4f4efccb401b859
Maxwell_li <3158918082@qq.com>
2024-10-31 14:32:05 +0800

    111

--- src/main/java/com/example/demo/HelloServlet.java
+++ src/main/java/com/example/demo/HelloServlet.java
@@ -26,7 +26,7 @@ public class HelloServlet extends HttpServlet {
         } else if ("python".equalsIgnoreCase(keyword)) {
             result = "查询结果：Python是一种易于学习的编程语言。";
         } else {
-            result = "11111";
+            result = "2211";
         }
 
         response.getWriter().write(result);
